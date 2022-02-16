a22828e1870895dd3fd855f8abf4988b747519ae
Lucia <luciaaroca02@gmail.com>
2022-02-16 20:03:00 +0000

    Actualizacion PruebaDespliegue

---
+++ areaCirculo.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AreaCirculo {
+
+    public static void main(String[] args) throws IOException {
+
+        final double PI = 3.146;  // declarando una constante
+        double area;  // declaración de variables
+        double radio;
+        // configuración del bufer de entrada
+        BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
+        String entrada;
+
+        // Entrada(radio)
+        System.out.println("Escribe el valor del radio: ");
+        entrada = bufer.readLine();
+        radio = Double.parseDouble(entrada);
+
+        // proceso
+        area = PI * radio * radio;
+
+        // Salida(area)
+        System.out.println("El área del círculo es: " + area );
+    }
+}
